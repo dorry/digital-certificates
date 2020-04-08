1154d528bc7306cd51a88eb67f1ada8126a54e5b
Sherif Abd El Khalek <32877807+SherifElNayad@users.noreply.github.com>
2020-04-09 00:29:00 +0200

    Add certificate is now a child component for dashboard
    As titled stated

--- asu-application/university-client/src/components/Dashboard.vue
+++ asu-application/university-client/src/components/Dashboard.vue
@@ -1,20 +1,26 @@
 <template>
 <div>
   <div class=" vertical-nav" >
-    <div class="horizontal-line"> </div>
+    <div class="horizontal-line">
+         </div>
     <b-nav vertical >
         <b-nav-item active>All Certificates</b-nav-item>
         <b-nav-item>Add Certificate</b-nav-item>
         <b-nav-item>Register Admin</b-nav-item>
         <b-nav-item >Statistics</b-nav-item>
     </b-nav>
+    <add-cert style="margin-top:-150px"> </add-cert>
+
 </div>
 </div>
 </template>
 
 <script>
+import add from "./AddCertificate.vue";
 export default {
-
+components:{
+    "addCert":add
+  },
 }
 </script>
 
